26befd55e6e0e03fbb534ab62f3d249a92a3aa3d
unknown <diego.racero@gmail.com>
2021-09-04 19:40:31 -0300

    routerrepair

--- rutas.js
+++ rutas.js
@@ -1,9 +1,9 @@
-import express, { Router } from 'express';
+import express from 'express';
 import cors from "cors";
 
 // creo una app de tipo express
 const app = express();
-const router = new Router();
+const router = express.Router();
 app.use(cors());
 app.use(express.json());
 app.use('/api/productos', router);
